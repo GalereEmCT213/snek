e94fa12c14990f9806f19089920da191737901c2
RinaCarvalho <rina.carvalho381@gmail.com>
2023-07-19 23:23:05 -0300

    Fix bug and coment figure

--- snek/learning/dql_agent.py
+++ snek/learning/dql_agent.py
@@ -88,7 +88,8 @@ class DQNAgent(Agent):
 
     def interact(self, state):
         if np.random.rand() < self.epsilon:
-            return random.randrange(self.action_size)
+            self.next_direction = self.moves[random.randrange(self.action_size)]
+            return
 
         actions = self.model.predict(state, verbose=0)
         actions = actions.reshape(-1)
